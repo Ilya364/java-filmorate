83811c04ff06231497668eef1c96692d922d9d99
ilya364 <tsh335nextYeAr@yandex.ru>
2023-12-06 23:11:57 +0300

    feat: добавить классы модели и их контроллеры

--- src/main/java/ru/yandex/practicum/filmorate/annotation/MinimumDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/MinimumDateValidator.java
@@ -6,12 +6,12 @@ import java.time.LocalDate;
 @SuppressWarnings("checkstyle:Regexp")
 public class MinimumDateValidator implements ConstraintValidator<MinimumDate, LocalDate> {
     private LocalDate earliestDate;
-    
+
     @Override
     public void initialize(MinimumDate constraintAnnotation) {
         earliestDate = LocalDate.parse(constraintAnnotation.value());
     }
-    
+
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
         return value == null || !value.isBefore(earliestDate);
